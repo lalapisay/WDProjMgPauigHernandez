ad2ce871806efa84b06bed395e5231a4ea528a7f
lalapisay <b2029laepauig@pshs.edu.ph>
2025-12-15 22:44:07 +0800

    guess whos gonna wake up at 3 for this -- Lala

--- js files/quiz.js
+++ js files/quiz.js
@@ -1,2 +1,32 @@
-// add const later
+const easy = {
+    question: "ah",
+    option: ["what", "the", "helly"]
+}
 
+const tanongbox = document.getElementById("question");
+const pilibox = document.getElementById("option");
+
+let questionownum = 0;
+let sinagutanmonaba = false;
+
+function check(){
+    if (questionownum >= easy.length){
+        tanongbox.textContent = `Yippee!! done!`;
+        pilibox.textContent = "";
+        return;
+    }
+}
+
+sinagutanmonaba = false; 
+const questionow = easy[questionownum];
+tanongbox.textContent = questionow.questionText;
+
+pilibox.innerHTML = "";
+
+for (let i = 0; i < questionow.option.length; i++){
+    const L = questionow.option.length[i];
+    const optionElement = document.createElement("div");
+
+}
+
+//IM GOING NUTS WHAT IF THIS IS THE LAST THING I'LL DO//
